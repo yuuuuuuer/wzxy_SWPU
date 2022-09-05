9e1ecf1d00c1c427a1dee3cb390654df146fb963
yuuuuuuer <93902917+yuuuuuuer@users.noreply.github.com>
2022-09-05 22:20:11 +0800

    Update 我在校园手机端自动打卡模拟.py

--- 我在校园手机端自动打卡模拟.py
+++ 我在校园手机端自动打卡模拟.py
@@ -12,7 +12,7 @@ def main():
             "Accept-Encoding": "gzip, deflate",
             "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7"}
     dk_url="https://gw.wozaixiaoyuan.com/health/mobile/health/save?batch=5600001"#此url是抓包得到的打卡终点链接，最终数据交互通过此链接
-    body={"location":"中国/四川省/成都市/新津区/花源街道/SWPU/156/510118/156510100/510118004",#此处为需要交互的数据
+    body={"location":"中国/XX省/XX市/XX区/XX街道/SWPU/156/510118/156510100/510118004",#此处为需要交互的数据 locaion可以随便填，后面的数字是前面国家/省份/城市/行政区依次对应的编码，测试发现这一行乱填也能打卡成功
           "t1":"[\"无下列情况\"]",
           "t2":"无不适症状",
           "t3":"暑假已离校",
@@ -20,9 +20,9 @@ def main():
           "t5":"正常",
           "t6":"安全",
           "t7":"已全部接种（含加强针）",
-          "type":0,
+          "type":0,#此处两行的type 和locationtype暂时不清楚是做什么的。
           "locationType":0}
-    r=requests.post(url=dk_url,data=json.dumps(body),headers=header)
+    r=requests.post(url=dk_url,data=json.dumps(body),headers=header)#此处的body要用json库进行dumps是因为前面抓包得到的header"Contene-Type"参数是json,意味着此时交换数据采用json格式.
     print(r)
 if __name__=="__main__":
     main()
